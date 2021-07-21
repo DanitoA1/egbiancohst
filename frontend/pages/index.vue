28ed072b5b3376ee8f15557a551d6763c7991b2a
Daniel Ahiaba <ahiabadaniel@gmail.com>
2021-07-21 16:59:10 +0100

    Initial Project Setup

---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Tutorial/>
+    <p class="my-10 text-center text-lg font-bold">Tailwind Classes at work here</p>
+    <div class="text-center my-2">
+      <el-button type="success">Element UI Button Working here</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
